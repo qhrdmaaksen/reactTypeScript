e7d4554563e973db7816827c8d6512addfc5d355
vitamin777 <rlaalsdn8@gmail.com>
2022-10-08 01:06:52 +0900

    refactor: 리액트와 타입스크립트로 함수형 컴포넌트 생성 및 활용
    -Todos.tsx 에서 React.FC 로 함수형 컴포넌트 명시 후 제네릭 타입 items 객체는 문자열 배열 명시 후 jsx 에서 props.items.map 으로 문자열 배열로 item 을 받도록하여
    App.tsx 에서 Todos component 의 속성에 props 로 들어갈 items 문자열 배열 값 명시하였음
    
    -정상 출력 확인

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
-import './App.css';
+import Todos from './components/Todos';
 
 function App() {
   return (
     <div className="App">
-
+      <Todos items={['리액트 공부', '타입스크립트 공부']} />
     </div>
   );
 }
